1e030534a5dc2879f6498cfb56201ffaa373e072
AneesaIbrahim <aneesaibrahim44@gmail.com>
2026-02-12 12:11:46 +0500

    Add files via upload

---
+++ Task 03.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double radius, area;
+	const double pi=3.14;
+	
+	cout<<"enter radius of the circle:";
+	cin>>radius;
+	
+	cout<<"area of the circle is:"<<area<<endl;
+
+	return 0;
+}
